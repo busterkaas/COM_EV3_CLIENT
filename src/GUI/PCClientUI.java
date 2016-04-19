51dd54bcf4fc50869cc34e575946f11929e84531
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2016-04-19 18:25:29 +0200

    fixed the gui layout and other minor changes + refactoring

--- src/GUI/PCClientUI.java
+++ src/GUI/PCClientUI.java
@@ -1,14 +1,18 @@
 package GUI;
 
-import java.awt.FlowLayout;
-import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,117 +20,168 @@ import Logic.PCClient;
 
 public class PCClientUI extends JFrame {
 
-	JPanel mainPanel;
-	JButton btnForwards, btnBackwards, btnRight, btnLeft, btnStop, btnQuit;
 	JTextField middlemanIpInput;
+	JButton btnForward, btnBack, btnRight, btnLeft, btnStop, btnConnect, btnDisconnect;
+	JPanel mainPanel;
 	PCClient client;
 	String command;
 
 	public PCClientUI() {
-		super("Robotics");
-		middlemanIpInput = new JTextField();
-		setupFrame();
-
-	}
-
-	private void setupFrame() {
-		setLayout(new FlowLayout());
-		setSize(800, 800);
-		setLocationRelativeTo(null);
-
-		mainPanel = new JPanel(new FlowLayout());
-
+		super("PC -> MIDDLEMAN -> SLAVE");
+		setupTextField();
 		setupButtons();
-		middlemanIpInput.setText("MOJN");
-		
-		
-		mainPanel.add(middlemanIpInput);
-		
-		mainPanel.add(btnForwards);
-
-		mainPanel.add(btnBackwards);
-
-		mainPanel.add(btnLeft);
-
-		mainPanel.add(btnRight);
-
-		mainPanel.add(btnStop);
-
-		mainPanel.add(btnQuit);
-
-		add(mainPanel);
-
+		setupPanel();
+		pack();
+		setupFrame();
 	}
 
-	private void setupButtons() {
-		// Buttons
-		btnForwards = new JButton("Forward");
-		btnForwards.addActionListener(new ActionListener() {
+	// BRUG EVT BAGEFTER DENNE TIL AT GIVE BESKEDER I, SÅSOM connected, ev3
+	// says... osv. INDTIL DER TRYKKES PÅ "DISCONNECT".
+	private void setupTextField() {
+		middlemanIpInput = new JTextField("EV3 ip-address");
+		Color colorRed = Color.BLACK;
+		Color colorGreen = Color.GREEN;
+		middlemanIpInput.setForeground(colorGreen);
+		middlemanIpInput.setBackground(colorRed);
+		middlemanIpInput.setHorizontalAlignment(middlemanIpInput.CENTER);
 
+		middlemanIpInput.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				middlemanIpInput.setText("");
+			}
+
+			public void mouseExited(MouseEvent e) {
+				if (middlemanIpInput.getText().equals(""))
+					middlemanIpInput.setText("IP");
+			}
+		});
+	}
+
+	// button : forward
+	private void setupButtons() {
+		btnForward = new JButton("FORWARD");
+		btnForward.setPreferredSize(new Dimension(120, 80));
+		btnForward.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				command = "forward";
 				informMiddleman();
-
 			}
 		});
 
-		btnBackwards = new JButton("Backward");
-		btnBackwards.addActionListener(new ActionListener() {
-
+		// button : back
+		btnBack = new JButton("BACK");
+		btnBack.setPreferredSize(new Dimension(120, 80));
+		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				command = "backward";
+				command = "back";
 				informMiddleman();
-
 			}
 		});
 
-		btnRight = new JButton("Go Right");
+		// button : right
+		btnRight = new JButton("RIGHT");
+		btnRight.setPreferredSize(new Dimension(120, 80));
 		btnRight.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent arg0) {
 				command = "right";
 				informMiddleman();
-
 			}
 		});
 
-		btnLeft = new JButton("Go left");
+		// button : left
+		btnLeft = new JButton("LEFT");
+		btnLeft.setPreferredSize(new Dimension(120, 80));
 		btnLeft.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent arg0) {
 				command = "left";
 				informMiddleman();
-
 			}
 		});
 
-		btnStop = new JButton("Stop");
+		// button : stop
+		btnStop = new JButton("STOP");
+		btnStop.setPreferredSize(new Dimension(120, 80));
 		btnStop.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent arg0) {
 				command = "stop";
 				informMiddleman();
-
 			}
 		});
 
-		btnQuit = new JButton("Quit");
-		btnQuit.addActionListener(new ActionListener() {
-
+		// button : connect
+		btnConnect = new JButton("CONNECT");
+		btnConnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				command = "quit";
+				command = "connect";
 				informMiddleman();
-
 			}
 		});
+
+		// button : disconnect
+		btnDisconnect = new JButton("DISCONNECT");
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				command = "disconnect";
+				informMiddleman();
+			}
+		});
+	}
+
+	private void setupFrame() {
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setResizable(false);
+		middlemanIpInput.requestFocus();
+	}
+
+	private void setupPanel() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new GridBagLayout());
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(15, 15, 15, 15);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		mainPanel.add(btnForward, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		mainPanel.add(btnLeft, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		mainPanel.add(btnStop, gbc);
+
+		gbc.gridx = 2;
+		gbc.gridy = 1;
+		mainPanel.add(btnRight, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		mainPanel.add(btnBack, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		mainPanel.add(middlemanIpInput, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		mainPanel.add(btnConnect, gbc);
+
+		gbc.gridx = 2;
+		gbc.gridy = 3;
+		mainPanel.add(btnDisconnect, gbc);
+
+		add(mainPanel);
 	}
 
 	private void informMiddleman() {
 		try {
 			client.sendMessage(command);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Error: " + e.getMessage());
 		}
 	}
-
 }
